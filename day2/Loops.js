5bf7a124615333131ac13b65148e891acd1b6696
Mert ERGÜDEN <mfurkanergdn@hotmail.com>
2021-03-17 11:04:17 +0300

    added Day2 Codes

---
+++ day2/Loops.js
@@ -0,0 +1,16 @@
+  var vowels = ['a', 'e', 'i', 'o', 'u']
+    for (var i = 0; i < s.length; i++)
+        {
+            if (vowels.indexOf(s[i]) > -1)
+                {
+                    console.log(s[i]);
+                }
+        }
+    
+    for (var j = 0; j < s.length; j++)
+        {
+            if (vowels.indexOf(s[j]) < 0)
+                {
+                    console.log(s[j]);
+                }
+        }
